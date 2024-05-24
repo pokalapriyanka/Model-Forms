3d0e5faa47057d24ccf9ee29ebc2ee911a81f7a3
pokalapriyanka <pokalapriyanka123@gmail.com>
2024-05-24 23:57:04 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -12,9 +12,19 @@ class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
         fields='__all__'
+        #fields=['topic_name','name']
+        #exclude=['email']
+        labels={'topic_name':'TN','name':'N'}
+        widgets={'url':forms.PasswordInput,'name':forms.Textarea}
+
 
 class AccessRecordForm(forms.ModelForm):
     class Meta:
         model=AccessRecord
         fields='__all__'
+        #fields=['name','date']
+        #exclude=['name']  
+        labels={'name':'N','author':'A'}
+        widgets={'author':forms.Textarea}
+
 
